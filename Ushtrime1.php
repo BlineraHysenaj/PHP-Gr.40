0f9498bd6ac52fc3d011e19e1655a0744fdd2173
BlineraHysenaj <52405935+BlineraHysenaj@users.noreply.github.com>
2020-05-07 22:50:33 +0200

    Header

---
+++ Ushtrime1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Lidhja me css -->    
+        <link rel="stylesheet" href="../u1/css/style1.css" type="text/css">
+    </head>
+    <body>
+        <!-- Header -->
+        <div id="bg-nav" style="height:478px;">
+            <header class="header-section">
+                <div class="container-fluid">
+                    <div class="row bg-overlay">
+                        <div class="header fadeInDown">
+                            <div class="col-lg-12">
+                                <div class="navigation mobile-menu">
+                                    <div class="main-menu">
+                                        <nav>
+                                            <ul>
+                                                <li><a class="active" href=""><i class="fa fa-home" aria-hidden="true"></i> Homepage</a></li>
+                                                <li><a href=""><i class="fas fa-comments"></i> About us</a></li>
+                                                <li><a href=""><i class="fas fa-users"></i> Education</a></li>
+                                               
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                    <div class="top-social">
+                                        <a href="https://www.facebook.com/groups/fiek.students" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                                        
+                                        <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=someone@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+                                        <a href="" class="admin-btn"><i class="fas fa-user-lock"></i></a>
+                                    </div>
+                                </div>
+                                <div id="mobile-menu-wrap"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                <div class="row">
+               
+                    <div class="col-lg-12 col-12">
+                <h3 class="weektext slideInDown"><span style="font-size:38px;font-weight:600">E</span>ducation <span style="font-size:38px;font-weight:600">D</span>epartment</h3>
+                    </div>
+                    <div class="col-lg-12 col-12">
+                <hr id="hr7" class="hr3 slideInRight">
+                    </div>
+                </div>
+                    </div>
+              
