a1727a5b4c7692ce0340f03f35fee92dc1a36a4a
BletaGashi <58531818+BletaGashi@users.noreply.github.com>
2020-05-07 22:34:49 +0200

    Footer

---
+++ Footer.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="../u1/css/style1.css" type="text/css">
+    </head>
+    <body>
+<footer>
+            <div class="footeri-lart" data-aos="fade-up">
+                <div class="wrapper">
+                    <div class="container-fluid">
+
+                        <div class="row">
+
+                            <div class="col col-12 col-lg-4  footeri-kontakti wow fadeInDown">
+                                <h3>Address :</h3>
+                                <p><i class="fas fa-map-marker-alt"></i>Bregu i Diellit, p.n.10000 Prishtinë Republika e Kosovës</p>
+                            </div>
+
+                            <div class="col col-12 col-lg-4  footeri-kontakti wow fadeInDown">
+                                <h3>Contact :</h3>
+                                <p><i class="fas fa-phone"></i> +381(0)38 554 896   </p>
+                            </div>
+
+                            <div class="col col-12 col-lg-4  footeri-kontakti wow fadeInDown">
+                                <h3>Email :</h3>
+                                <p><i class="fas fa-envelope"></i> <a href="mailto:noreply@uni-pr.edu">noreply@uni-pr.edu</a></p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="footeri-posht">
+                    <div class="container">
+                        <div class="row" >
+                            <iframe class="mapa" src="https://www.google.com/maps/dir/Bregu+i+Diellit,+p.n.10000+Prishtin%C3%AB+Republika+e+Kosov%C3%ABs/Bregu+i+Diellit,+Prishtina/@42.6524542,21.1741165,251m/data=!3m1!1e3!4m9!4m8!1m0!1m5!1m1!1s0x13549ec5989c3e7b:0xdce360181b819e84!2m2!1d21.1755651!2d42.6523948!3e2?hl=en" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+                            <div class="col footeri-mediasociale">
+                                <a href="https://www.facebook.com/groups/fiek.students" target="_blank"><i class="fab fa-facebook-f"></i></a> 
+                               
+                                <a href="https://github.com/BlineraHysenaj/PHP-Gr.40"><i class="fab fa-github" target="_blank"></i></a>
+
+                            </div>
+                        </div>
+                    </div><br>
+                    <p style="text-align: center; filter: opacity(50%)">GR.40 &copy; All rights reserved 2020 -<a href="https://github.com/BlineraHysenaj/PHP-Gr.40" target="_blank"> </a></p>
+
+                </div>
+            </div>
+
+        </footer>
+        </body>
+        </html>
