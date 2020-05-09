a4d3f893536a6e20d4fd3635b53b4dfe3f6321ea
BleronaIdrizi <58502639+BleronaIdrizi@users.noreply.github.com>
2020-05-09 10:23:20 +0200

    Shtimi i fotove dhe disa linqeve

---
+++ Webometrix.php
@@ -0,0 +1,31 @@
+<div class="jumbotron nditext">
+<div class="wrapper">
+    <div class="container-fluid nditextwidth">
+<div class="row">
+        <div class="media"  data-aos="fade-up">
+            <div class="col-12 col-lg-12">
+                <p style="text-align:center;"><strong>Webometric</strong></p>
+            <img class="fotologot" src="../2020/Photos/ndilogo.png" width="180px" height="100px" alt="k">
+            <div class="media-body"></div>
+                <h5 style="text-align: center;" class="mt-0"></h5>
+                <p>
+                The Webometrics Ranking of World Universities, also known as Ranking Web of Universities, is a ranking system for the world's universities based on a composite indicator that takes into account both the volume of the Web content (number of web pages and files) and the visibility and impact of these web publications according to the number of external inlinks (site citations) they received. 
+                  
+            </div>
+        </div>
+
+</style>
+</head>
+
+        <div class="media"  data-aos="fade-up">
+                        <div class="col-12 col-lg-12">
+
+            <img class="fotologot"id="blina"  src="../2020/Photos/5.jpg" width="464px" height="138px" alt="Generic placeholder image"> <br>
+            <img class="fotologot"id="test"  src="../2020/Photos/6.jpg" width="464px" height="138px" alt="Generic placeholder image">
+            
+
+                            <div class="media-body"></div>
+     
+            </div>
+        </div>
+        </div></div></div></div>
