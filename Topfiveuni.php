ac48dead69ae7c9f38ae927785f03106d9e5bc41
BlineraHysenaj <52405935+BlineraHysenaj@users.noreply.github.com>
2020-05-09 02:23:32 +0200

    Top Five Universities

---
+++ Topfiveuni.php
@@ -0,0 +1,102 @@
+
+<!--UNIVERSITIES-TOP 3-->
+
+<div id="schedule" class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="titulli-div">
+                        <h3 class="titulli"><span>TOP </span> <span style="color: #dd0a37;">FIVE UNIVERSITIES</span></h3>
+                    </div>
+
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="events-wrapper">
+                            <!-- Universiteti 1 top-->
+                            <div class="eventi"  data-aos="fade-up">
+                                <div class="dita-eventit">
+                                    <div>
+                                    <p><i style="font-size: 50px" class="fas fa-trophy "></i></p>
+                                    </div>
+                                </div>
+                                <div class="permbajtja-eventit">
+                                    <p class="koha-eventit"><i class="fa fa-clock-o"></i> </p>
+                                    <h5 class="titulli-eventit"> <a href="https://www.topuniversities.com/university-rankings-articles/world-university-rankings/top-universities-world-2020">Massachusetts Institute of Technology</h5>
+                                    
+                                    <p>United States </a></p>
+                                </div>
+                            </div>
+                            <!-- Mbarimi  -->
+
+                            <!-- Universiteti 2 -->
+                           <div class="eventi" data-aos="fade-up">
+                                <div class="dita-eventit">
+                                    <div>
+                                    <p><i style="font-size: 50px" class="fas fa-award "></i></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="permbajtja-eventit">
+                                    
+                                    <h3 class="titulli-eventit"><a href="https://www.topuniversities.com/university-rankings-articles/world-university-rankings/top-universities-world-2020">Stanford University</h5>
+                                    <p>United States </a></p>
+                                </div>
+                            </div>
+                            <!-- Mbarimi -->
+
+                           <!-- Universiteti 3 -->
+                           <div class="eventi" data-aos="fade-up">
+                                <div class="dita-eventit">
+                                    <div>
+                                    <p><i style="font-size: 50px" class="fas fa-check "></i></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="permbajtja-eventit">
+                                    
+                                    <h3 class="titulli-eventit"><a href="https://www.topuniversities.com/university-rankings-articles/world-university-rankings/top-universities-world-2020">Harvard University</h5>
+                                    <p>United States </a></p>
+                                </div>
+                            </div>
+                            <!-- Mbarimi -->
+                             <!-- Universiteti 4 -->
+                           <div class="eventi" data-aos="fade-up">
+                                <div class="dita-eventit">
+                                    <div>
+                                    <p><i style="font-size: 50px" class="fas fa-check-square "></i></p>
+                                      
+                                        
+                                    </div>
+                                </div>
+                                <div class="permbajtja-eventit">
+                                    
+                                    <h3 class="titulli-eventit"><a href="https://www.topuniversities.com/university-rankings-articles/world-university-rankings/top-universities-world-2020">University of Oxford</h5>
+                                    <p>United Kingdom </a></p>
+                                </div>
+                            </div>
+                            <!-- Mbarimi -->
+
+                                <!-- Universiteti 5-->
+                           <div class="eventi" data-aos="fade-up">
+                                <div class="dita-eventit">
+                                    <div>
+                                    <p><i style="font-size: 50px" class="fas fa-check-circle "></i></p>
+                                      
+                                        
+                                    </div>
+                                </div>
+                                <div class="permbajtja-eventit">
+                                    
+                                    <h3 class="titulli-eventit"><a href="https://www.topuniversities.com/university-rankings-articles/world-university-rankings/top-universities-world-2020">California Institute of Technology</h5>
+                                    <p>United States </a></p>
+                                </div>
+                            </div>
+                            <!-- Mbarimi -->
+                        </div>
+                        <div class="col-12 col-md-12 col-md-offset-2" data-aos="fade-up">
+                            <a href="../2020/databaza/KerkesateProjektit.txt" download><div class="btn butoni3"><i style="color: #FFF" class="fa fa-download"></i> Download
+                                </div></a></div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+<br><br>
