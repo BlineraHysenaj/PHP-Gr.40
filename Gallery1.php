c21f10bbd7810458e11f952c875486f5b9bd5d33
BletaGashi <58531818+BletaGashi@users.noreply.github.com>
2020-05-09 14:54:58 +0200

    Galeria nr 1

---
+++ Gallery1.php
@@ -0,0 +1,35 @@
+   <!-- Galleria numer 1 -->
+   <div class="col col-12 col-lg-6 col-md-6 col-sm-6"  data-aos="fade-up">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="../2020/Photos/Dita1/foto1.jpg" >
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="../2020/Photos/Dita1/foto2.jpg" >
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="../2020/Photos/Dita1/foto6.jpg" >
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="../2020/Photos/Dita1/foto.jpg" >
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+
+                </div></div></div>   
