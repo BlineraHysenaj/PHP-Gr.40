82ba146e287d07fe92d4d6243da2c72c2146a9e7
BlineraHysenaj <52405935+BlineraHysenaj@users.noreply.github.com>
2020-05-12 03:35:31 +0200

    Konfigurimi me databaze

---
+++ konfigurimiConst.php
@@ -0,0 +1,15 @@
+<?php
+
+//Adresa e serverit që default është localhost.
+define('DB_SERVER', 'localhost'); 
+
+//Useri i localhostit që default është root.
+define('DB_USERNAME', 'root'); 
+
+//Passwordi që default është i zbrazet.
+define('DB_PASSWORD', ''); 
+
+//Emri i databazës tuaj.
+define('DB_NAME', 'database'); 
+
+?>
