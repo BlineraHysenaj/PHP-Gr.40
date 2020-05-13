6cbf2011819abb36c03d293ad4e76c10877fa7c3
BletaGashi <58531818+BletaGashi@users.noreply.github.com>
2020-05-13 15:26:17 +0200

    Headre.php

---
+++ Header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+        <!-- Fillimi i Bootstrap -->
+        <link rel="stylesheet" type="text/css" href="bt-content/css/bootstrap.min.css">
+        <script src="bt-content/js/bootstrap.min.js"></script>
+
+        <!-- Css i jashtum -->
+        <link rel="stylesheet" type="text/css" href="bt-content/css/style.css">
+
+        <!--Librari per animacione -->
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />    
+
+
+
+    </head>
+    <body>
+        
+      <!-- Krijimi i navigacionit dhe backgroundit -->
+        <div class="bg-nav">
+
+            <div class="pos-f-t">
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <div class="bg-light p-4">
+
+                        <ul class="navbar-nav ">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home <span class="sr-only">(current)</span></a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./aboutUs.php"><i class="fa fa-users" aria-hidden="true"></i> About Us</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="./register.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Sign in</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="./login.php"><i class="fa fa-unlock" aria-hidden="true"></i> Log in</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+                     <!-- Krijimi i navigacionit per burger -->
+                     <nav class="navbar ikonaNav">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i>
+                        </span>
+                    </button>
+                </nav>
+            </div> 
+            
+            </div>
+            </div>
+            <div> <br> <br>
