2bd4c8c88d04c30c77f2c491503c3383346b6aa0
BletaGashi <58531818+BletaGashi@users.noreply.github.com>
2020-05-13 08:29:20 +0200

    Update kycu.php

--- kycu.php
+++ kycu.php
@@ -97,4 +97,108 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     echo "Please try again later!";
                 }
             }
+            // Mbyll deklaraten (statement).
+            mysqli_stmt_close($stmt);
+        }
+        
+        else{
+        // Shfaq një mesazh gabimi nëse email-a nuk është valide.
+            $username_err = "Ju lutem shkruani email-in tuaj valid!";
+        }
+        
+    }
+   
+    // Mbyll koneksionin (lidhjen).
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Admin - Week of Women &copy; </title>
+    <link rel="icon" type="image/png" href="../2020/Photos/logo.png">
+    <link rel="apple-touch-icon" type="image/png" href="../2020/Photos/logo.png" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.0/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <style type="text/css">
+        .center {
+            margin: auto;
+            margin-top: 100px;
+            width: 100%;
+            padding: 10px;
+        }
+
+        .form-group {
+            width: 30%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        #bg-nav {
+            width: 100%;
+            height: 100%;
+            background-image: url("bt-content/Images/100.jpg");
+            background-size: cover;
+            color: #FFF;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .form-group {
+                width: 100%;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+        }
+    </style>
+</head>
+
+<body id="bg-nav">
+
+    <div class="center">
+        <center>
+            <h2>Administrator</h2>
+        </center>
+
+        <center>
+            <p>Only specific people are allowed to enter this page.</p>
+        </center>
+
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Admin:</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>
+
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password:</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+
+            <div class="form-group">
+                <center><input type="submit" class="btn btn-success" value="Log in">
+                </center>
+            </div>
+
+        </form>
+    </div>
+    <center><a href="./index.php"><button class="btn btn-primary">Return Home</button></a></center>
+
+
+    <script src="js/particles.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+
+</html>
              
