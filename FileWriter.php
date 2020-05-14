390c2e90ee9c8ae9573c2b03301f4b21bebb2944
BlineraHysenaj <52405935+BlineraHysenaj@users.noreply.github.com>
2020-05-14 03:36:13 +0200

    Create FileWriter.php

---
+++ FileWriter.php
@@ -0,0 +1,18 @@
+<?php
+
+$filename = "D:/FileExport.php";
+$file = fopen ("$filename", "w");
+
+fwrite($file, " Ky tekst eshte lexuar nga file 'FileExport.php'.");
+if(file_exists($filename)){
+	$filesize = filesize($filename);
+	$msg = "Fajlli u krijua me emrin '$filename'.";
+	
+	echo ($msg);
+}
+else
+{
+	echo ("Fajlli me emrin '$filename' nuk ekziston.");
+}
+
+?>
