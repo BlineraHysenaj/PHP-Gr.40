25253ddf02b30b687fc73ec92a51b5f202fd1ca1
BletaGashi <52405935+BlineraHysenaj@users.noreply.github.com>
2020-05-17 22:17:44 +0200

    Footer Final

--- Footer.php
+++ Footer.php
@@ -1,16 +1,15 @@
-
 <footer>
     <!--Krijimi i foterit -->
     <div class="container-fluid foteri">
         <div class="row" data-aos="zoom-in">
 
-        <div class="col col-lg-4 col-12 ">
-                        <h1><i class="fa fa-location-arrow" aria-hidden="true"></i> Address :</h1>
-                        <!-- Substring -->
-                        <?php
-                        echo substr("Bregu i Diellit- 1000 Prishtina Kosovo",0,15)."<br>"; ?>
-                      <!--  <p>Bregu i Diellit- 1000 Prishtina Kosovo</p>-->
-                    </div>
+            <div class="col col-lg-4 col-12 ">
+                <h1><i class="fa fa-location-arrow" aria-hidden="true"></i> Address :</h1>
+                <!-- Substring -->
+                <?php
+                echo substr("Bregu i Diellit- 1000 Prishtina Kosovo", 0, 15) . "<br>"; ?>
+                <!--  <p>Bregu i Diellit- 1000 Prishtina Kosovo</p>-->
+            </div>
 
             <div class="col col-lg-4 col-12 ">
                 <h1><i class="fa fa-phone-square" aria-hidden="true"></i> Contact :</h1>
@@ -23,34 +22,27 @@
             </div>
 
             <div class="col col-lg-12 col-12">
-                
+
                 <!--Paraqitja e iframe te mapit -->
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1471.753372737915!2d21.044426257954523!3d42.45950997992854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13537f00b9312eab%3A0x64cc0dfa29a9062e!2sDonje%20Godance!5e0!3m2!1sen!2s!4v1588196537271!5m2!1sen!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
-
-
     </div>
 
 </footer>
-
-
-
-<!-- Fillimi i librarive te JS -->  
+<!-- Fillimi i librarive te JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="bt-content/js/bootstrap.min.js"></script>
 
-
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 <script>
     AOS.init({
-
-        offset: 120, 
+        offset: 120,
         delay: 0,
         once: true,
-
     });
 </script>
+
 </body>
 </html>
