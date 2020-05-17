aa324f8281187b1a5c794f7ab1f357809d94f587
BletaGashi <58531818+BletaGashi@users.noreply.github.com>
2020-05-17 16:06:56 +0200

    Update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -47,8 +47,8 @@ require("deletecookies.php");
   
   date_default_timezone_set('america/new_york');
   
-  $aMember = new Perdoruesi( "Blerona", "Skenderaj", 'http://w3programmers.com/' );
-  echo $aMember->getUsername() . " jeton ne " . $aMember->getLocation() ."<br>";
+  $aMember = new Perdoruesi( "FIEK", "Bregun e Diellit", 'http://w3programmers.com/' );
+  echo $aMember->getUsername() . " gjendet ne " . $aMember->getLocation() ."<br>";
   $aMember->save();
   $aTopic = new Koha( "Koha: ", $aMember );
   $aTopic->showHeader();
